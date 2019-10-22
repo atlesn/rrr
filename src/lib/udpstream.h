71747bf70ea5a31344421361e8bb95c63f297ba3
Atle Solbakken <atle@goliathdns.no>
2019-10-23 00:26:46 +0200

    Further improvements and bugfixes in UDPstream ACK-system.
    - Regulation of send window size regulated by received in ACK packets
    - Update docs

--- src/lib/udpstream.h
+++ src/lib/udpstream.h
@@ -31,19 +31,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "ip.h"
 
 #define RRR_UDPSTREAM_VERSION 1
-#define RRR_UDPSTREAM_BUFFER_MAX 250
+#define RRR_UDPSTREAM_BUFFER_MAX 1500
 #define RRR_UDPSTREAM_BURST_RECEIVE_MAX 100000
-//#define RRR_UDPSTREAM_FRAME_SIZE_MAX 1024
-#define RRR_UDPSTREAM_DATA_SIZE_MAX 20
+#define RRR_UDPSTREAM_DATA_SIZE_MAX 1024
 #define RRR_UDPSTREAM_TIMEOUT_MS 5000
+#define RRR_UDPSTREAM_INVALID_TIMEOUT_MS (RRR_UDPSTREAM_TIMEOUT_MS*2)
 #define RRR_UDPSTREAM_RESEND_INTERVAL_MS 1000
 #define RRR_UDPSTREAM_FRAME_ID_MAX 4294967295
 #define RRR_UDPSTREAM_UNACKNOWLEDGED_LIMIT 5
 #define RRR_UDPSTREAM_SEND_BURST_LIMIT 100
 #define RRR_UDPSTREAM_SEND_SIZE_MAX 67584
-#define RRR_UDPSTREAM_WINDOW_SIZE_MIN 75
-#define RRR_UDPSTREAM_WINDOW_SIZE_INITIAL RRR_UDPSTREAM_FRAME_ID_MAX/2
-#define RRR_UDPSTREAM_WINDOW_SIZE_MAX 25000
+#define RRR_UDPSTREAM_WINDOW_SIZE_MIN 10
+#define RRR_UDPSTREAM_WINDOW_SIZE_MAX RRR_UDPSTREAM_BUFFER_MAX*2
+#define RRR_UDPSTREAM_WINDOW_SIZE_INITIAL RRR_UDPSTREAM_WINDOW_SIZE_MAX/4
 
 #define RRR_UDPSTREAM_OK 0
 #define RRR_UDPSTREAM_ERR 1
@@ -59,6 +59,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RRR_UDPSTREAM_FRAME_FLAGS_FRAME_ACK		(1<<3)
 #define RRR_UDPSTREAM_FRAME_FLAGS_DELIVERY_ACK	(1<<4)
 #define RRR_UDPSTREAM_FRAME_FLAGS_RELEASE_ACK	(1<<5)
+#define RRR_UDPSTREAM_FRAME_FLAGS_WINDOW_SIZE	(1<<6)
 
 #define RRR_UDPSTREAM_FRAME_FLAGS(frame) \
 	((frame)->flags)
@@ -86,7 +87,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RRR_UDPSTREAM_FRAME_PACKED_ACK_LAST(frame) \
 	(be32toh((frame)->ack_data.ack_id_last))
 #define RRR_UDPSTREAM_FRAME_PACKED_BOUNDARY_ID(frame) \
-	(be64toh((frame)->boundary_id))
+	(be64toh((frame)->boundary_id_combined))
 #define RRR_UDPSTREAM_FRAME_PACKED_DATA_CRC32(frame) \
 	(be32toh((frame)->data_crc32))
 
@@ -102,20 +103,28 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	((RRR_UDPSTREAM_FRAME_FLAGS(frame) & RRR_UDPSTREAM_FRAME_FLAGS_DELIVERY_ACK) != 0)
 #define RRR_UDPSTREAM_FRAME_IS_RELEASE_ACK(frame) \
 	((RRR_UDPSTREAM_FRAME_FLAGS(frame) & RRR_UDPSTREAM_FRAME_FLAGS_RELEASE_ACK) != 0)
+#define RRR_UDPSTREAM_FRAME_IS_WINDOW_SIZE(frame) \
+	((RRR_UDPSTREAM_FRAME_FLAGS(frame) & RRR_UDPSTREAM_FRAME_FLAGS_WINDOW_SIZE) != 0)
 
-#define RRR_UDPSTREAM_HEADER_FIELDS 			\
-	uint8_t flags;								\
-	uint8_t version;							\
-	uint16_t stream_id;							\
-	union {										\
-		uint32_t frame_id;						\
-		uint32_t connect_handle;				\
-	};											\
-	union {										\
-		struct rrr_udpstream_ack_data ack_data; \
-		uint64_t boundary_id;					\
-	};											\
+#define RRR_UDPSTREAM_HEADER_FIELDS 							\
+	uint8_t flags;												\
+	uint8_t version;											\
+	uint16_t stream_id;											\
+	union {														\
+		uint32_t frame_id;										\
+		uint32_t connect_handle;								\
+		uint32_t window_size;									\
+	};															\
+	union {														\
+		struct rrr_udpstream_ack_data ack_data; 				\
+		uint64_t boundary_id_combined;							\
+	};															\
 	uint16_t data_size
+
+struct rrr_udpstream_boundary_id {
+	uint32_t boundary_high;
+	uint32_t boundary_low;
+};
 
 struct rrr_udpstream_ack_data {
 	uint32_t ack_id_first;
@@ -159,10 +168,12 @@ struct rrr_udpstream_stream {
 	struct rrr_udpstream_frame_buffer send_buffer;
 	uint16_t stream_id;
 	uint32_t connect_handle;
+	uint32_t boundary_id_high;
 	struct sockaddr *remote_addr;
 	socklen_t remote_addr_len;
 	uint64_t last_seen;
-	int64_t window_size;
+	uint32_t window_size_to_remote;
+	uint32_t window_size_from_remote;
 	int invalidated;
 };
 
@@ -175,14 +186,11 @@ struct rrr_udpstream_stream_collection {
 struct rrr_udpstream {
 	struct ip_data ip;
 	int flags;
-	struct rrr_udpstream_stream_collection streams;
 
+	struct rrr_udpstream_stream_collection streams;
 	struct rrr_socket_read_session_collection read_sessions;
 
 	pthread_mutex_t lock;
-
-	// Used when receiving connections, find a free ID fast (hopefully)
-	uint16_t next_stream_id;
 
 	void *send_buffer;
 	ssize_t send_buffer_size;
@@ -238,14 +246,15 @@ int rrr_udpstream_release_ack_urge (
 		uint16_t stream_id,
 		uint64_t boundary_id,
 		const struct sockaddr *addr,
-		socklen_t addr_len
+		socklen_t addr_len,
+		int window_size_change
 );
 int rrr_udpstream_queue_outbound_data (
 		struct rrr_udpstream *udpstream_data,
 		uint32_t connect_handle,
 		const void *data,
 		ssize_t data_size,
-		uint64_t boundary_identifier
+		uint64_t boundary_id
 );
 void rrr_udpstream_close (
 		struct rrr_udpstream *data
@@ -256,12 +265,14 @@ int rrr_udpstream_bind (
 );
 int rrr_udpstream_connect_raw (
 		uint32_t *connect_handle,
+		uint32_t boundary_id_low,
 		struct rrr_udpstream *data,
 		struct sockaddr *addr,
 		socklen_t socklen
 );
 int rrr_udpstream_connect (
 		uint32_t *connect_handle,
+		uint32_t boundary_id_high,
 		struct rrr_udpstream *data,
 		const char *remote_host,
 		const char *remote_port
