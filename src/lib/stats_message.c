34bdbcf687805f052ee4e7320888af5c95486784
Atle Solbakken <atle@goliathdns.no>
2020-03-21 18:32:42 +0100

    Fix incorrect endian.h on FreeBSD + add rrr_stats to gitignore

--- src/lib/stats_message.c
+++ src/lib/stats_message.c
@@ -23,8 +23,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
-#include <endian.h>
 
+#include "rrr_endian.h"
 #include "../global.h"
 #include "stats_message.h"
 #include "rrr_socket_read.h"
