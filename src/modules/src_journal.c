d295312d0c6a630f70a4c59718c0e34ceb4938b5
Atle Solbakken <atle@goliathdns.no>
2020-08-06 02:01:49 +0200

    Performance improvements in journal module
    * Prevent messages building up in delivery queue if a module is spamming
      log messages. Force the thread to sleep if queue fills up.
    * More statistics to see whats going on
    * Improve main thread sleep algorithm

--- src/modules/src_journal.c
+++ src/modules/src_journal.c
@@ -49,6 +49,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RRR_JOURNAL_TOPIC_PREFIX "rrr/journal"
 #define RRR_JOURNAL_HOSTNAME_MAX_LEN 256
 
+// Other threads must sleep when queue is full
+#define RRR_JOURNAL_DELIVERY_QUEUE_SLEEP_LIMIT 5000
+#define RRR_JOURNAL_DELIVERY_QUEUE_SLEEP_TIME_MS 5
+
 struct journal_queue_entry {
 	RRR_LL_NODE(struct journal_queue_entry);
 	uint64_t timestamp;
@@ -67,6 +71,8 @@ struct journal_data {
 
 	pthread_mutex_t delivery_lock;
 	struct journal_queue delivery_queue;
+	uint64_t delivery_queue_sleep_event_count;
+
 	int is_in_hook;
 	int error_in_hook;
 
@@ -187,8 +193,20 @@ static void journal_log_hook (
 ) {
 	struct journal_data *data = private_arg;
 
+	// Make the calling thread pause a bit to reduce the amount of messages
+	// coming in. This is done if we are unable to handle request due to
+	// slowness of the readers of journal module. DO NOT sleep inside the
+	// lock, that would make things worse by making journal module unable
+	// to empty the delivery queue
+	int do_sleep_before_return = 0;
+
 	// This is a recursive lock
 	pthread_mutex_lock(&data->delivery_lock);
+
+	if (RRR_LL_COUNT(&data->delivery_queue) > RRR_JOURNAL_DELIVERY_QUEUE_SLEEP_LIMIT) {
+		do_sleep_before_return = 1;
+		data->delivery_queue_sleep_event_count++;
+	}
 
 	struct journal_queue_entry *entry = NULL;
 
@@ -241,11 +259,21 @@ static void journal_log_hook (
 			journal_queue_entry_destroy(entry);
 		}
 		pthread_mutex_unlock(&data->delivery_lock);
+		if (do_sleep_before_return) {
+			rrr_posix_usleep(RRR_JOURNAL_DELIVERY_QUEUE_SLEEP_TIME_MS * 1000);
+		}
 		return;
 }
 
+struct journal_write_message_callback_data {
+	struct journal_data *data;
+	int entry_count;
+	int entry_count_limit;
+};
+
 static int journal_write_message_callback (struct rrr_msg_msg_holder *entry, void *arg) {
-	struct journal_data *data = arg;
+	struct journal_write_message_callback_data *callback_data = arg;
+	struct journal_data *data = callback_data->data;
 
 	int ret = 0;
 
@@ -307,7 +335,9 @@ static int journal_write_message_callback (struct rrr_msg_msg_holder *entry, voi
 
 	reading = NULL;
 
-	if (RRR_LL_COUNT(&data->delivery_queue) > 0) {
+	callback_data->entry_count++;
+
+	if (RRR_LL_COUNT(&data->delivery_queue) > 0 && callback_data->entry_count < callback_data->entry_count_limit) {
 		ret = RRR_MESSAGE_BROKER_AGAIN;
 	}
 
@@ -379,6 +409,8 @@ static void *thread_entry_journal (struct rrr_thread *thread) {
 
 	uint64_t next_test_msg_time = 0;
 
+	uint64_t prev_delivery_queue_sleep_event_count = 0;
+
 	while (!rrr_thread_check_encourage_stop(thread_data->thread)) {
 		rrr_thread_update_watchdog_time(thread_data->thread);
 
@@ -397,6 +429,12 @@ static void *thread_entry_journal (struct rrr_thread *thread) {
 			}
 		}
 
+		struct journal_write_message_callback_data callback_data = {
+			data,
+			0,
+			400
+		};
+
 		if (rrr_message_broker_write_entry (
 				INSTANCE_D_BROKER(thread_data),
 				INSTANCE_D_HANDLE(thread_data),
@@ -404,7 +442,7 @@ static void *thread_entry_journal (struct rrr_thread *thread) {
 				0,
 				0,
 				journal_write_message_callback,
-				data
+				&callback_data
 		)) {
 			RRR_MSG_0("Could not create new message in journal instance %s\n",
 					INSTANCE_D_NAME(thread_data));
@@ -412,17 +450,53 @@ static void *thread_entry_journal (struct rrr_thread *thread) {
 		}
 
 		if (time_now - time_start > 1000000) {
+			int output_buffer_count = 0;
+			int delivery_ratelimit_active = 0;
+			uint64_t delivery_queue_sleep_event_count = 0;
+			int delivery_queue_count = 0;
+
+			if (rrr_instance_default_set_output_buffer_ratelimit_when_needed (
+					&output_buffer_count,
+					&delivery_ratelimit_active,
+					thread_data
+			) != 0) {
+				RRR_MSG_ERR("Error while setting ratelimit in journal instance %s\n",
+						INSTANCE_D_NAME(thread_data));
+				break;
+			}
+
+			pthread_mutex_lock(&data->delivery_lock);
+			delivery_queue_sleep_event_count = data->delivery_queue_sleep_event_count;
+			delivery_queue_count = RRR_LL_COUNT(&data->delivery_queue);
+			pthread_mutex_unlock(&data->delivery_lock);
+
 			time_start = time_now;
 			rrr_stats_instance_update_rate (INSTANCE_D_STATS(thread_data), 0, "processed", data->count_processed - prev_processed);
 			rrr_stats_instance_update_rate (INSTANCE_D_STATS(thread_data), 1, "suppressed", data->count_suppressed - prev_suppressed);
 			rrr_stats_instance_update_rate (INSTANCE_D_STATS(thread_data), 2, "total", data->count_total - prev_total);
+			rrr_stats_instance_update_rate (INSTANCE_D_STATS(thread_data), 3, "delivery_queue_sleeps", delivery_queue_sleep_event_count - prev_delivery_queue_sleep_event_count);
+			rrr_stats_instance_post_unsigned_base10_text (
+					INSTANCE_D_STATS(thread_data),
+					"output_buffer_count",
+					0,
+					output_buffer_count
+			);
+			rrr_stats_instance_post_unsigned_base10_text (
+					INSTANCE_D_STATS(thread_data),
+					"delivery_queue_count",
+					0,
+					delivery_queue_count
+			);
 
+			prev_delivery_queue_sleep_event_count = delivery_queue_sleep_event_count;
 			prev_processed = data->count_processed;
 			prev_suppressed = data->count_suppressed;
 			prev_total = data->count_total;
 		}
 
-		rrr_posix_usleep (50000); // 50 ms
+		if (callback_data.entry_count == 0) {
+			rrr_posix_usleep (50000); // 50 ms
+		}
 	}
 
 	out_cleanup:
