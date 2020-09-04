fd47bc651079f929000134df962e47ed37f29a07
Atle Solbakken <atle@goliathdns.no>
2020-09-04 13:39:15 +0200

    Add rpm build script.

---
+++ misc/rpm/make_rpm.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+TMPDIR=~
+VERSION=`cat version`
+TGZ=rrr-$VERSION.tar.gz
+SPEC=rrr.spec
+
+mkdir -p $TMPDIR || exit 1
+
+rm -rf $TMPDIR/rpmbuild || exit 1
+rm -f $TMPDIR/$TGZ || exit 1
+rm -f $TMPDIR/$SPEC || exit 1
+
+cp $SPEC $TMPDIR || exit 1
+
+cd ../../ || exit 1
+TRANSFORM=s,^,rrr-$VERSION/,
+tar --transform $TRANSFORM -chzf $TMPDIR/$TGZ *
+
+cd $TMPDIR && rpmdev-setuptree && mv $SPEC rpmbuild/SPECS/ && mv $TGZ rpmbuild/SOURCES/ || exit 1
+
+rpmbuild -ba rpmbuild/SPECS/$SPEC || exit 1
+
+echo "RPM build tree complete in $TMPDIR"
