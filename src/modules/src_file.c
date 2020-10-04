00c257f0f09a31b301b0c54fcf13e64dc7c5ba9b
Atle Solbakken <atle@goliathdns.no>
2020-10-04 12:54:48 +0200

    Add structured file read all functionallity in file module. Add to test suite.

--- src/modules/src_file.c
+++ src/modules/src_file.c
@@ -26,6 +26,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 
 #include "../lib/log.h"
 
@@ -70,6 +73,7 @@ struct file {
 	char *orig_path;
 	char *real_path;
 	int fd;
+	struct stat file_stat;
 	uint64_t total_messages;
 	uint64_t last_read_time;
 };
@@ -78,14 +82,24 @@ struct file_collection {
 	RRR_LL_HEAD(struct file);
 };
 
+enum file_read_method {
+	FILE_READ_METHOD_TELEGRAMS,
+	FILE_READ_METHOD_ALL_SIMPLE,
+	FILE_READ_METHOD_ALL_STRUCTURED
+};
+
 struct file_data {
 	struct rrr_instance_runtime_data *thread_data;
 
 	struct rrr_array_tree *tree;
 	int do_try_keyboard_input;
 	int do_no_keyboard_hijack;
-	int do_read_all_to_message;
 	int do_unlink_on_close;
+
+	int do_read_all_to_message_;
+	char *read_all_method;
+
+	enum file_read_method read_method;
 
 	char *directory;
 	char *prefix;
@@ -134,7 +148,8 @@ static int file_collection_push (
 		int flags,
 		const char *orig_path,
 		const char *real_path,
-		int fd
+		int fd,
+		const struct stat *file_stat
 ) {
 	int ret = 0;
 
@@ -163,6 +178,7 @@ static int file_collection_push (
 	file->type = type;
 	file->fd = fd;
 	file->flags = flags;
+	file->file_stat = *file_stat;
 
 	RRR_LL_PUSH(files, file);
 	file = NULL;
@@ -186,6 +202,7 @@ static void file_data_cleanup(void *arg) {
 	if (data->tree != NULL) {
 		rrr_array_tree_destroy(data->tree);
 	}
+	RRR_FREE_IF_NOT_NULL(data->read_all_method);
 	RRR_FREE_IF_NOT_NULL(data->directory);
 	RRR_FREE_IF_NOT_NULL(data->prefix);
 	RRR_FREE_IF_NOT_NULL(data->topic);
@@ -223,9 +240,12 @@ static int file_parse_config (struct file_data *data, struct rrr_instance_config
 		}
 	}
 
+
+	RRR_INSTANCE_CONFIG_PARSE_OPTIONAL_YESNO("file_read_all_to_message", do_read_all_to_message_, 0);
+	RRR_INSTANCE_CONFIG_PARSE_OPTIONAL_UTF8_DEFAULT_NULL("file_read_all_method", read_all_method);
+
 	RRR_INSTANCE_CONFIG_PARSE_OPTIONAL_YESNO("file_try_keyboard_input", do_try_keyboard_input, 0);
 	RRR_INSTANCE_CONFIG_PARSE_OPTIONAL_YESNO("file_no_keyboard_hijack", do_no_keyboard_hijack, 0);
-	RRR_INSTANCE_CONFIG_PARSE_OPTIONAL_YESNO("file_read_all_to_message", do_read_all_to_message, 0);
 	RRR_INSTANCE_CONFIG_PARSE_OPTIONAL_YESNO("file_unlink_on_close", do_unlink_on_close, 0);
 
 	RRR_INSTANCE_CONFIG_PARSE_OPTIONAL_UNSIGNED("file_max_messages_per_file", max_messages_per_file, 0);
@@ -235,6 +255,34 @@ static int file_parse_config (struct file_data *data, struct rrr_instance_config
 
 	/* Don't goto out in errors, check all possible errors first. */
 
+	if (data->do_read_all_to_message_) {
+		data->read_method = FILE_READ_METHOD_ALL_SIMPLE;
+	}
+	else {
+		data->read_method = FILE_READ_METHOD_TELEGRAMS;
+	}
+
+	if (RRR_INSTANCE_CONFIG_EXISTS("file_read_all_method")) {
+		if (!data->do_read_all_to_message_) {
+			RRR_MSG_0("Parameter file_read_all_method was set while file_read_all_to_message was not 'yes' in file instance %s, this is a configuration error.\n",
+					config->name);
+			ret = 1;
+		}
+
+		if (rrr_posix_strcasecmp(data->read_all_method, "simple") == 0) {
+			data->read_method = FILE_READ_METHOD_ALL_SIMPLE;
+		}
+		else if (rrr_posix_strcasecmp(data->read_all_method, "structured") == 0) {
+			data->read_method = FILE_READ_METHOD_ALL_STRUCTURED;
+
+		}
+		else {
+			RRR_MSG_0("Unknown value '%s' for file_read_all_method in file instance %s, valid options are 'simple' and 'structured'.\n",
+					config->name);
+			ret = 1;
+		}
+	}
+
 	if (data->max_open > RRR_FILE_MAX_MAX_OPEN) {
 		RRR_MSG_0("Parameter file_max_open out of range for file instance %s (%" PRIrrrbl ">%i).\n",
 				config->name, data->max_open, RRR_FILE_MAX_MAX_OPEN);
@@ -242,7 +290,7 @@ static int file_parse_config (struct file_data *data, struct rrr_instance_config
 	}
 
 	if (	!RRR_INSTANCE_CONFIG_EXISTS("file_input_types") &&
-			data->do_read_all_to_message == 0 &&
+			data->do_read_all_to_message_ == 0 &&
 			data->do_unlink_on_close == 0
 	) {
 		RRR_MSG_0("No actions defined in configuration for file instance %s, one or more must be specified\n", config->name);
@@ -250,13 +298,13 @@ static int file_parse_config (struct file_data *data, struct rrr_instance_config
 	}
 
 	if (	RRR_INSTANCE_CONFIG_EXISTS("file_input_types") &&
-			data->do_read_all_to_message != 0
+			data->do_read_all_to_message_ != 0
 	) {
 		RRR_MSG_0("Both file_input_types and do_read_all_to_message was set in file instance %s, this is a configuration error.\n", config->name);
 		ret = 1;
 	}
 
-	if (data->do_read_all_to_message && data->max_messages_per_file != 0) {
+	if (data->do_read_all_to_message_ && data->max_messages_per_file != 0) {
 		RRR_MSG_0("Both file_do_read_all_to_message and file_max_messages_per_file was set in file instance %s, this is a configuration error.\n", config->name);
 		ret = 1;
 	}
@@ -267,7 +315,7 @@ static int file_parse_config (struct file_data *data, struct rrr_instance_config
 	}
 
 	if (	RRR_INSTANCE_CONFIG_EXISTS("file_max_read_step_size") &&
-			data->do_read_all_to_message != 0
+			data->do_read_all_to_message_ != 0
 	) {
 		RRR_MSG_0("Both file_max_read_step_size and do_read_all_to_message was set in file instance %s, this is a configuration error.\n", config->name);
 		ret = 1;
@@ -360,7 +408,15 @@ static int file_probe_callback (
 		}
 	}
 
-	if ((ret = file_collection_push(&data->files, type, flags, orig_path, resolved_path, fd)) != 0) {
+	struct stat file_stat = {0};
+	if (fstat(fd, &file_stat) != 0) {
+		RRR_MSG_0("Warning: Failed to stat file %s: %s\n",
+				resolved_path, rrr_strerror(errno));
+		ret = 1;
+		goto out;
+	}
+
+	if ((ret = file_collection_push(&data->files, type, flags, orig_path, resolved_path, fd, &file_stat)) != 0) {
 		goto out;
 	}
 
@@ -420,13 +476,13 @@ static int file_read_array_write_callback (struct rrr_msg_holder *entry, void *a
 	return ret;
 }
 
-struct file_read_array_callback_data {
+struct file_read_callback_data {
 	struct file_data *file_data;
 	struct file *file;
 };
 
 static int file_read_array_callback (struct rrr_read_session *read_session, struct rrr_array *array_final, void *arg) {
-	struct file_read_array_callback_data *callback_data = arg;
+	struct file_read_callback_data *callback_data = arg;
 
 	int ret = 0;
 
@@ -478,14 +534,16 @@ static int file_read_all_to_message_get_target_size_callback (
 }
 
 struct file_read_all_to_message_write_callback_data {
-	struct file_data *data;
+	struct file_data *file_data;
+	struct file *file;
 	const struct rrr_read_session *read_session;
 };
 
-static int file_read_all_to_message_write_callback (struct rrr_msg_holder *entry, void *arg) {
-	struct file_read_all_to_message_write_callback_data *callback_data = arg;
-	struct file_data *data = callback_data->data;
-
+static int file_read_all_to_message_write_callback_simple (
+		struct rrr_msg_holder *entry,
+		struct file_data *file_data,
+		const struct rrr_read_session *read_session
+) {
 	int ret = 0;
 
 	uint64_t time = rrr_time_get_64();
@@ -496,24 +554,137 @@ static int file_read_all_to_message_write_callback (struct rrr_msg_holder *entry
 			MSG_TYPE_MSG,
 			MSG_CLASS_DATA,
 			time,
-			data->topic_len,
-			callback_data->read_session->rx_buf_wpos
+			file_data->topic_len,
+			read_session->rx_buf_wpos
 	)) != 0) {
-		RRR_MSG_0("Could not create message in file_read_all_to_message_write_callback\n");
+		RRR_MSG_0("Could not create message in file_read_all_to_message_write_callback_simple\n");
 		goto out;
 	}
 
-	if (data->topic != NULL && *(data->topic) != '\0') {
-		memcpy(MSG_TOPIC_PTR(reading), data->topic, data->topic_len);
+	if (file_data->topic != NULL && *(file_data->topic) != '\0') {
+		memcpy(MSG_TOPIC_PTR(reading), file_data->topic, file_data->topic_len);
 	}
 
-	memcpy(MSG_DATA_PTR(reading), callback_data->read_session->rx_buf_ptr, callback_data->read_session->rx_buf_wpos);
+	memcpy(MSG_DATA_PTR(reading), read_session->rx_buf_ptr, read_session->rx_buf_wpos);
 
 	entry->message = reading;
 	entry->data_length = MSG_TOTAL_SIZE(reading);
 
-	RRR_DBG_2("file instance %s created message with raw data of size %lu and timestamp %" PRIu64 "\n",
-			INSTANCE_D_NAME(callback_data->data->thread_data), callback_data->read_session->rx_buf_wpos, time);
+	RRR_DBG_2("file instance %s created message with raw file_data of size %lu and timestamp %" PRIu64 "\n",
+			INSTANCE_D_NAME(file_data->thread_data), read_session->rx_buf_wpos, time);
+
+	out:
+	return ret;
+}
+
+static int file_read_all_to_message_write_callback_structured (
+		struct rrr_msg_holder *entry,
+		struct file_data *file_data,
+		const struct rrr_read_session *read_session,
+		struct file *file
+) {
+	int ret = 0;
+
+	struct rrr_array array_tmp = {0};
+
+	if ((ret = rrr_array_push_value_blob_with_tag_with_size (
+			&array_tmp, "data", read_session->rx_buf_ptr, read_session->rx_buf_wpos
+	)) != 0) {
+		RRR_MSG_0("Failed to push file data to array in file_read_all_to_message_write_callback_structured\n");
+		goto out;
+	}
+
+	if ((ret = rrr_array_push_value_u64_with_tag (
+			&array_tmp, "size", read_session->rx_buf_wpos
+	)) != 0) {
+		RRR_MSG_0("Failed to push file size to array in file_read_all_to_message_write_callback_structured\n");
+		goto out;
+	}
+
+	if ((ret = rrr_array_push_value_str_with_tag (
+			&array_tmp, "path_original", file->orig_path
+	)) != 0) {
+		RRR_MSG_0("Failed to push file original path to array in file_read_all_to_message_write_callback_structured\n");
+		goto out;
+	}
+
+	if ((ret = rrr_array_push_value_str_with_tag (
+			&array_tmp, "path_resolved", file->real_path
+	)) != 0) {
+		RRR_MSG_0("Failed to push file resolved path to array in file_read_all_to_message_write_callback_structured\n");
+		goto out;
+	}
+
+	if ((ret = rrr_array_push_value_i64_with_tag (
+			&array_tmp, "atime", file->file_stat.st_atim.tv_sec
+	)) != 0) {
+		RRR_MSG_0("Failed to push file atime to array in file_read_all_to_message_write_callback_structured\n");
+		goto out;
+	}
+
+	if ((ret = rrr_array_push_value_i64_with_tag (
+			&array_tmp, "mtime", file->file_stat.st_mtim.tv_sec
+	)) != 0) {
+		RRR_MSG_0("Failed to push file mtime to array in file_read_all_to_message_write_callback_structured\n");
+		goto out;
+	}
+
+	if ((ret = rrr_array_push_value_i64_with_tag (
+			&array_tmp, "ctime", file->file_stat.st_ctim.tv_sec
+	)) != 0) {
+		RRR_MSG_0("Failed to push file ctime to array in file_read_all_to_message_write_callback_structured\n");
+		goto out;
+	}
+
+	uint64_t time = rrr_time_get_64();
+
+	struct rrr_msg_msg *reading = NULL;
+	if ((ret = rrr_array_new_message_from_collection (
+			&reading,
+			&array_tmp,
+			time,
+			file_data->topic,
+			file_data->topic_len
+	)) != 0) {
+		RRR_MSG_0("Could not create array message in file_read_all_to_message_write_callback_structured\n");
+		goto out;
+	}
+
+	entry->message = reading;
+	entry->data_length = MSG_TOTAL_SIZE(reading);
+
+	RRR_DBG_2("file instance %s created message with structured raw file_data of size %lu and timestamp %" PRIu64 "\n",
+			INSTANCE_D_NAME(file_data->thread_data), read_session->rx_buf_wpos, time);
+
+	out:
+	rrr_array_clear(&array_tmp);
+	return ret;
+}
+
+static int file_read_all_to_message_write_callback (struct rrr_msg_holder *entry, void *arg) {
+	struct file_read_all_to_message_write_callback_data *callback_data = arg;
+
+	int ret = 0;
+
+	if (callback_data->file_data->read_method == FILE_READ_METHOD_ALL_SIMPLE) {
+		if ((ret = file_read_all_to_message_write_callback_simple (
+				entry,
+				callback_data->file_data,
+				callback_data->read_session
+		)) != 0) {
+			goto out;
+		}
+	}
+	else {
+		if ((ret = file_read_all_to_message_write_callback_structured (
+				entry,
+				callback_data->file_data,
+				callback_data->read_session,
+				callback_data->file
+		)) != 0) {
+			goto out;
+		}
+	}
 
 	out:
 	rrr_msg_holder_unlock(entry);
@@ -524,12 +695,14 @@ static int file_read_all_to_message_complete_callback (
 		struct rrr_read_session *read_session,
 		void *arg
 ) {
-	struct file_data *data = arg;
+	struct file_read_callback_data *callback_data = arg;
+	struct file_data *data = callback_data->file_data;
 
 	int ret = 0;
 
-	struct file_read_all_to_message_write_callback_data callback_data = {
+	struct file_read_all_to_message_write_callback_data write_callback_data = {
 			data,
+			callback_data->file,
 			read_session
 	};
 
@@ -539,7 +712,7 @@ static int file_read_all_to_message_complete_callback (
 			0,
 			0,
 			file_read_all_to_message_write_callback,
-			&callback_data
+			&write_callback_data
 	)) != 0) {
 		RRR_MSG_0("Could not create new message in file instance %s, return was %i\n",
 				INSTANCE_D_NAME(data->thread_data), ret);
@@ -585,38 +758,42 @@ static int file_read (uint64_t *bytes_read, struct file_data *data, struct file 
 		socket_flags |= RRR_SOCKET_READ_INPUT_DEVICE;
 	}
 
-	struct file_read_array_callback_data receive_array_callback_data = {
+	struct file_read_callback_data read_callback_data = {
 		data,
 		file
 	};
 
-	if (data->tree != NULL && (ret = rrr_socket_common_receive_array_tree (
-			bytes_read,
-			&file->read_session_collection,
-			file->fd,
-			socket_flags,
-			&array_final,
-			data->tree,
-			0,
-			data->max_read_step_size,
-			RRR_FILE_MAX_SIZE_MB * 1024 * 1024,
-			file_read_array_callback,
-			&receive_array_callback_data
-	)) != 0) {
-		if (ret == RRR_READ_INCOMPLETE) {
-			ret = 0;
+	if (data->read_method == FILE_READ_METHOD_TELEGRAMS) {
+		if (data->tree == NULL) {
+			RRR_BUG("BUG: No array tree was present for read method TELEGRAMS in file_read\n");
+		}
+		if ((ret = rrr_socket_common_receive_array_tree (
+				bytes_read,
+				&file->read_session_collection,
+				file->fd,
+				socket_flags,
+				&array_final,
+				data->tree,
+				0,
+				data->max_read_step_size,
+				RRR_FILE_MAX_SIZE_MB * 1024 * 1024,
+				file_read_array_callback,
+				&read_callback_data
+		)) != 0) {
+			if (ret == RRR_READ_INCOMPLETE) {
+				ret = 0;
+				goto out;
+			}
+			if (ret != RRR_READ_EOF) {
+				RRR_MSG_0("Warning: Failed while reading array data from file '%s'=>'%s' in file instance %s\n",
+						file->orig_path, file->real_path, INSTANCE_D_NAME(data->thread_data));
+			}
 			goto out;
 		}
-		if (ret != RRR_READ_EOF) {
-			RRR_MSG_0("Warning: Failed while reading array data from file '%s'=>'%s' in file instance %s\n",
-					file->orig_path, file->real_path, INSTANCE_D_NAME(data->thread_data));
-		}
-		goto out;
 	}
-
-	file->last_read_time = rrr_time_get_64();
-
-	if (data->do_read_all_to_message) {
+	else if (data->read_method == FILE_READ_METHOD_ALL_SIMPLE ||
+			data->read_method == FILE_READ_METHOD_ALL_STRUCTURED
+	) {
 		if (data->tree != NULL) {
 			RRR_BUG("BUG: Two methods was specified in file_read, config parser should check for this\n");
 		}
@@ -630,9 +807,9 @@ static int file_read (uint64_t *bytes_read, struct file_data *data, struct file 
 				RRR_FILE_MAX_SIZE_MB * 1024 * 1024,
 				socket_flags,
 				file_read_all_to_message_get_target_size_callback,
-				data,
+				NULL,
 				file_read_all_to_message_complete_callback,
-				data
+				&read_callback_data
 		)) != 0) {
 			if (ret == RRR_READ_EOF) {
 				// Close file
@@ -652,9 +829,11 @@ static int file_read (uint64_t *bytes_read, struct file_data *data, struct file 
 			ret = RRR_READ_SOFT_ERROR;
 		}
 	}
-	else if (data->tree == NULL) {
-		RRR_BUG("BUG: No methods was specified in file_read, config parser should check for this\n");
+	else {
+		RRR_BUG("BUG: Unknown read_method %i in file_read\n", data->read_method);
 	}
+
+	file->last_read_time = rrr_time_get_64();
 
 	out:
 	rrr_array_clear(&array_final);
