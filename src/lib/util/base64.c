1539da50e191948925f7e37adcceb80efcc65af6
Atle Solbakken <atle@goliathdns.no>
2020-11-06 17:23:46 +0100

    Move function in file.

--- src/lib/util/base64.c
+++ src/lib/util/base64.c
@@ -89,56 +89,6 @@ unsigned char * rrr_base64_encode(const unsigned char *src, size_t len,
 }
 
 
-static const unsigned char base64url_table[65] =
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
-
-/*
- * Same as above function but base64url scheme is used (no newlines, no =, and +/ becomes -_)
- */
-unsigned char * rrr_base64url_encode(const unsigned char *src, size_t len,
-			      size_t *out_len)
-{
-	unsigned char *out, *pos;
-	const unsigned char *end, *in;
-	size_t olen;
-
-	olen = len * 4 / 3 + 4; /* 3-byte blocks to 4-byte */
-	olen += olen / 72; /* line feeds */
-	olen++; /* nul termination */
-	if (olen < len)
-		return NULL; /* integer overflow */
-	out = malloc(olen);
-	if (out == NULL)
-		return NULL;
-
-	end = src + len;
-	in = src;
-	pos = out;
-	while (end - in >= 3) {
-		*pos++ = base64url_table[in[0] >> 2];
-		*pos++ = base64url_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
-		*pos++ = base64url_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
-		*pos++ = base64url_table[in[2] & 0x3f];
-		in += 3;
-	}
-
-	if (end - in) {
-		*pos++ = base64url_table[in[0] >> 2];
-		if (end - in == 1) {
-			*pos++ = base64url_table[(in[0] & 0x03) << 4];
-		} else {
-			*pos++ = base64url_table[((in[0] & 0x03) << 4) |
-					      (in[1] >> 4)];
-			*pos++ = base64url_table[(in[1] & 0x0f) << 2];
-		}
-	}
-
-	*pos = '\0';
-	if (out_len)
-		*out_len = pos - out;
-	return out;
-}
-
 /**
  * base64_decode - Base64 decode
  * @src: Data to be decoded
@@ -206,5 +156,55 @@ unsigned char * rrr_base64_decode(const unsigned char *src, size_t len,
 	}
 
 	*out_len = pos - out;
+	return out;
+}
+
+static const unsigned char base64url_table[65] =
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+
+/*
+ * Same as above function but base64url scheme is used (no newlines, no =, and +/ becomes -_)
+ */
+unsigned char * rrr_base64url_encode(const unsigned char *src, size_t len,
+			      size_t *out_len)
+{
+	unsigned char *out, *pos;
+	const unsigned char *end, *in;
+	size_t olen;
+
+	olen = len * 4 / 3 + 4; /* 3-byte blocks to 4-byte */
+	olen += olen / 72; /* line feeds */
+	olen++; /* nul termination */
+	if (olen < len)
+		return NULL; /* integer overflow */
+	out = malloc(olen);
+	if (out == NULL)
+		return NULL;
+
+	end = src + len;
+	in = src;
+	pos = out;
+	while (end - in >= 3) {
+		*pos++ = base64url_table[in[0] >> 2];
+		*pos++ = base64url_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
+		*pos++ = base64url_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
+		*pos++ = base64url_table[in[2] & 0x3f];
+		in += 3;
+	}
+
+	if (end - in) {
+		*pos++ = base64url_table[in[0] >> 2];
+		if (end - in == 1) {
+			*pos++ = base64url_table[(in[0] & 0x03) << 4];
+		} else {
+			*pos++ = base64url_table[((in[0] & 0x03) << 4) |
+					      (in[1] >> 4)];
+			*pos++ = base64url_table[(in[1] & 0x0f) << 2];
+		}
+	}
+
+	*pos = '\0';
+	if (out_len)
+		*out_len = pos - out;
 	return out;
 }
