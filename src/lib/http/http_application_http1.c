71a428d8060cac7aceb50fcb12c9e21c02719cfd
Atle Solbakken <atle@goliathdns.no>
2020-12-31 06:23:02 +0100

    Remove commented-out code.

--- src/lib/http/http_application_http1.c
+++ src/lib/http/http_application_http1.c
@@ -1005,35 +1005,6 @@ static int __rrr_http_application_http1_receive_get_target_size (
 
 	const char *end = read_session->rx_buf_ptr + read_session->rx_buf_wpos;
 
-	// ASCII validation
-/*	int rnrn_counter = 4;
-	for (const unsigned char *pos = (const unsigned char *) read_session->rx_buf_ptr; pos < (const unsigned char *) end; pos++) {
-//		printf("pos: %02x\n", *pos);
-		if (*pos == '\r' && (rnrn_counter == 4 || rnrn_counter == 2)) {
-			--rnrn_counter;
-		}
-		else if (*pos == '\n' && (rnrn_counter == 3 || rnrn_counter == 1)) {
-			if (--rnrn_counter == 0) {
-				break; // Header complete
-			}
-		}
-		else {
-			rnrn_counter = 4;
-
-			// TODO : Why do this? We should be OK with non-ASCII
-			if (*pos > 0x7f) {
-				RRR_MSG_0("Received non-ASCII character %02x in HTTP request\n", *pos);
-				ret = RRR_READ_SOFT_ERROR;
-				goto out;
-			}
-		}
-	}
-
-	if (rnrn_counter != 0) {
-		ret = RRR_READ_INCOMPLETE;
-		goto out;
-	}*/
-
 	size_t target_size;
 	size_t parsed_bytes = 0;
 
