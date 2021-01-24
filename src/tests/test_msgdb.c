dcadca2c3abbfd85d209dbb22c5a4593b88e7aea
Atle Solbakken <atle@goliathdns.no>
2021-01-25 00:20:50 +0100

    Fork and start message server in test suite.

--- src/tests/test_msgdb.c
+++ src/tests/test_msgdb.c
@@ -21,16 +21,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 #include "../lib/log.h"
 #include "../lib/msgdb/msgdb_client.h"
 #include "../lib/util/rrr_time.h"
 #include "../lib/util/macro_utils.h"
 #include "../lib/messages/msg_msg.h"
+#include "../lib/rrr_strerror.h"
+#include "../lib/util/posix.h"
+#include "test.h"
+
+#define MSGDB_CMD "../.libs/rrr_msgdb"
 
 int rrr_test_msgdb(void) {
 	int ret = 0;
 
+	pid_t msgserver_pid = 0;
 	struct rrr_msg_msg *msg = NULL;
 
 	const char msg_topic[] = "a/b/c";
@@ -46,6 +57,20 @@ int rrr_test_msgdb(void) {
 		goto out;
 	}
 
+	RRR_DBG_1("Forking to start message database service '" MSGDB_CMD "'...\n");
+
+	if ((msgserver_pid = fork()) < 0) {
+		TEST_MSG("Could not fork: %s\n", rrr_strerror(errno));
+		ret = 1;
+		goto out;
+	}
+	else if (msgserver_pid == 0) {
+		// Child code
+		execl(MSGDB_CMD, "", "-s", "/tmp/rrr_test_msgdb.sock", "-d", "/tmp/rrr_test_msgdb/", (char *) NULL);
+		TEST_MSG("Could not execute message database command " MSGDB_CMD ": %s\n", rrr_strerror(errno));
+		exit(1);
+	}
+
 	memcpy(MSG_TOPIC_PTR(msg), msg_topic, sizeof(msg_topic) - 1);
 
 	if ((ret = rrr_msgdb_client_put(msg)) != 0) {
@@ -53,6 +78,22 @@ int rrr_test_msgdb(void) {
 	}
 
 	out:
+	if (msgserver_pid > 0) {
+		rrr_posix_usleep(500000);
+		if (kill(msgserver_pid, SIGTERM) < 0) {
+			TEST_MSG("Failed while sending SIGTERM to message database server: %s\n", rrr_strerror(errno));
+			ret |= 1;
+		}
+		int status;
+		if (waitpid(msgserver_pid, &status, 0) < 0) {
+			TEST_MSG("Failed while waiting on message database server: %s\n", rrr_strerror(errno));
+			ret |= 1;
+		}
+		else if (WEXITSTATUS(status) != 0) {
+			TEST_MSG("Non-zero exit status %i from message database server\n", WEXITSTATUS(status));
+			ret |= 1;
+		}
+	}
 	RRR_FREE_IF_NOT_NULL(msg);
 	return ret;
 }
