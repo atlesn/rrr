3f56f40004aac47e5a49adcf06338fa9b4152a3a
Atle Solbakken <atle@goliathdns.no>
2021-01-29 08:14:50 +0100

    Cleanup indentaion.

--- src/lib/http/http_common.h
+++ src/lib/http/http_common.h
@@ -27,35 +27,35 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "../read_constants.h"
 
 #define RRR_HTTP_CLIENT_USER_AGENT "RRR/" PACKAGE_VERSION
+#define RRR_HTTP_SERVER_USER_AGENT "RRR/" PACKAGE_VERSION
 
-#define RRR_HTTP_CLIENT_TIMEOUT_STALL_MS	500
-#define RRR_HTTP_CLIENT_TIMEOUT_TOTAL_MS	2000
+#define RRR_HTTP_CLIENT_TIMEOUT_STALL_MS    500
+#define RRR_HTTP_CLIENT_TIMEOUT_TOTAL_MS    2000
 
-#define RRR_HTTP_SERVER_USER_AGENT						"RRR/" PACKAGE_VERSION
-#define RRR_HTTP_SERVER_WORKER_THREAD_WATCHDOG_TIMER_MS	5000
-#define RRR_HTTP_SERVER_WORKER_FIRST_DATA_TIMEOUT_MS	2000
-#define RRR_HTTP_SERVER_WORKER_IDLE_TIMEOUT_MS			30000
-#define RRR_HTTP_SERVER_WORKER_TRANSACTION_TIMEOUT_MS	RRR_HTTP_SERVER_WORKER_IDLE_TIMEOUT_MS
+#define RRR_HTTP_SERVER_WORKER_THREAD_WATCHDOG_TIMER_MS   5000
+#define RRR_HTTP_SERVER_WORKER_FIRST_DATA_TIMEOUT_MS      2000
+#define RRR_HTTP_SERVER_WORKER_IDLE_TIMEOUT_MS            30000
+#define RRR_HTTP_SERVER_WORKER_TRANSACTION_TIMEOUT_MS     RRR_HTTP_SERVER_WORKER_IDLE_TIMEOUT_MS
 
-#define RRR_HTTP_OK						RRR_READ_OK
-#define RRR_HTTP_HARD_ERROR				RRR_READ_HARD_ERROR
-#define RRR_HTTP_SOFT_ERROR				RRR_READ_SOFT_ERROR
-#define RRR_HTTP_BUSY						RRR_READ_INCOMPLETE
+#define RRR_HTTP_OK                      RRR_READ_OK
+#define RRR_HTTP_HARD_ERROR              RRR_READ_HARD_ERROR
+#define RRR_HTTP_SOFT_ERROR              RRR_READ_SOFT_ERROR
+#define RRR_HTTP_BUSY                    RRR_READ_INCOMPLETE
 
-#define RRR_HTTP_PARSE_OK				RRR_READ_OK
-#define RRR_HTTP_PARSE_HARD_ERR 		RRR_READ_HARD_ERROR
-#define RRR_HTTP_PARSE_SOFT_ERR			RRR_READ_SOFT_ERROR
-#define RRR_HTTP_PARSE_INCOMPLETE		RRR_READ_INCOMPLETE
-#define RRR_HTTP_PARSE_EOF				RRR_READ_EOF
+#define RRR_HTTP_PARSE_OK                RRR_READ_OK
+#define RRR_HTTP_PARSE_HARD_ERR          RRR_READ_HARD_ERROR
+#define RRR_HTTP_PARSE_SOFT_ERR          RRR_READ_SOFT_ERROR
+#define RRR_HTTP_PARSE_INCOMPLETE        RRR_READ_INCOMPLETE
+#define RRR_HTTP_PARSE_EOF               RRR_READ_EOF
 
-#define RRR_HTTP_RESPONSE_CODE_SWITCHING_PROTOCOLS		101
-#define RRR_HTTP_RESPONSE_CODE_OK						200
-#define RRR_HTTP_RESPONSE_CODE_OK_NO_CONTENT			204
-#define RRR_HTTP_RESPONSE_CODE_ERROR_BAD_REQUEST		400
-#define RRR_HTTP_RESPONSE_CODE_ERROR_NOT_FOUND			404
-#define RRR_HTTP_RESPONSE_CODE_INTERNAL_SERVER_ERROR	500
-#define RRR_HTTP_RESPONSE_CODE_GATEWAY_TIMEOUT			504
-#define RRR_HTTP_RESPONSE_CODE_VERSION_NOT_SUPPORTED	505
+#define RRR_HTTP_RESPONSE_CODE_SWITCHING_PROTOCOLS        101
+#define RRR_HTTP_RESPONSE_CODE_OK                         200
+#define RRR_HTTP_RESPONSE_CODE_OK_NO_CONTENT              204
+#define RRR_HTTP_RESPONSE_CODE_ERROR_BAD_REQUEST          400
+#define RRR_HTTP_RESPONSE_CODE_ERROR_NOT_FOUND            404
+#define RRR_HTTP_RESPONSE_CODE_INTERNAL_SERVER_ERROR      500
+#define RRR_HTTP_RESPONSE_CODE_GATEWAY_TIMEOUT            504
+#define RRR_HTTP_RESPONSE_CODE_VERSION_NOT_SUPPORTED      505
 
 #define RRR_HTTP_WEBSOCKET_GUID "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
 
@@ -101,11 +101,11 @@ extern const char *rrr_http_transport_str_any;
 extern const char *rrr_http_transport_str_http;
 extern const char *rrr_http_transport_str_https;
 
-#define RRR_HTTP_TRANSPORT_TO_STR(transport)												\
-	(transport == RRR_HTTP_TRANSPORT_ANY ? rrr_http_transport_str_any :						\
-	(transport == RRR_HTTP_TRANSPORT_HTTP ? rrr_http_transport_str_http :					\
-	(transport == RRR_HTTP_TRANSPORT_HTTPS ? rrr_http_transport_str_https : ("unknown")		\
-	)))
+#define RRR_HTTP_TRANSPORT_TO_STR(transport)                                             \
+    (transport == RRR_HTTP_TRANSPORT_ANY ? rrr_http_transport_str_any :                  \
+    (transport == RRR_HTTP_TRANSPORT_HTTP ? rrr_http_transport_str_http :                \
+    (transport == RRR_HTTP_TRANSPORT_HTTPS ? rrr_http_transport_str_https : ("unknown")  \
+    )))
 
 extern const char *rrr_http_method_str_get;
 extern const char *rrr_http_method_str_head;
@@ -118,55 +118,55 @@ extern const char *rrr_http_method_str_post_urlencoded_no_quoting;
 extern const char *rrr_http_method_str_post_application_octet_stream;
 extern const char *rrr_http_method_str_post_application_text_plain;
 
-#define RRR_HTTP_METHOD_TO_STR(method)																				\
-	(method == RRR_HTTP_METHOD_GET ? rrr_http_method_str_get :														\
-	(method == RRR_HTTP_METHOD_HEAD ? rrr_http_method_str_head :													\
-	(method == RRR_HTTP_METHOD_PUT ? rrr_http_method_str_put :														\
-	(method == RRR_HTTP_METHOD_DELETE ? rrr_http_method_str_delete :												\
-	(method == RRR_HTTP_METHOD_POST_MULTIPART_FORM_DATA ? rrr_http_method_str_post_multipart_form_data :			\
-	(method == RRR_HTTP_METHOD_POST_URLENCODED ? rrr_http_method_str_post_urlencoded :								\
-	(method == RRR_HTTP_METHOD_POST_URLENCODED_NO_QUOTING ? rrr_http_method_str_post_urlencoded_no_quoting :		\
-	(method == RRR_HTTP_METHOD_POST_APPLICATION_OCTET_STREAM ? rrr_http_method_str_post_application_octet_stream :	\
-	(method == RRR_HTTP_METHOD_POST_TEXT_PLAIN ? rrr_http_method_str_post_application_text_plain : ("unknown")		\
-	)))))))))
+#define RRR_HTTP_METHOD_TO_STR(method)                                                                                         \
+    (method == RRR_HTTP_METHOD_GET ? rrr_http_method_str_get :                                                                 \
+    (method == RRR_HTTP_METHOD_HEAD ? rrr_http_method_str_head :                                                               \
+    (method == RRR_HTTP_METHOD_PUT ? rrr_http_method_str_put :                                                                 \
+    (method == RRR_HTTP_METHOD_DELETE ? rrr_http_method_str_delete :                                                           \
+    (method == RRR_HTTP_METHOD_POST_MULTIPART_FORM_DATA ? rrr_http_method_str_post_multipart_form_data :                       \
+    (method == RRR_HTTP_METHOD_POST_URLENCODED ? rrr_http_method_str_post_urlencoded :                                         \
+    (method == RRR_HTTP_METHOD_POST_URLENCODED_NO_QUOTING ? rrr_http_method_str_post_urlencoded_no_quoting :                   \
+    (method == RRR_HTTP_METHOD_POST_APPLICATION_OCTET_STREAM ? rrr_http_method_str_post_application_octet_stream :             \
+    (method == RRR_HTTP_METHOD_POST_TEXT_PLAIN ? rrr_http_method_str_post_application_text_plain : ("unknown")                 \
+    )))))))))
 
-#define RRR_HTTP_METHOD_TO_STR_CONFORMING(method)																	\
-	(method == RRR_HTTP_METHOD_GET ? rrr_http_method_str_get :														\
-	(method == RRR_HTTP_METHOD_HEAD ? rrr_http_method_str_head :													\
-	(method == RRR_HTTP_METHOD_PUT ? rrr_http_method_str_put :														\
-	(method == RRR_HTTP_METHOD_DELETE ? rrr_http_method_str_delete :												\
-	rrr_http_method_str_post																						\
-	))))
+#define RRR_HTTP_METHOD_TO_STR_CONFORMING(method)                                                                              \
+    (method == RRR_HTTP_METHOD_GET ? rrr_http_method_str_get :                                                                 \
+    (method == RRR_HTTP_METHOD_HEAD ? rrr_http_method_str_head :                                                               \
+    (method == RRR_HTTP_METHOD_PUT ? rrr_http_method_str_put :                                                                 \
+    (method == RRR_HTTP_METHOD_DELETE ? rrr_http_method_str_delete :                                                           \
+    rrr_http_method_str_post                                                                                                   \
+    ))))
 
 extern const char *rrr_http_upgrade_mode_str_none;
 extern const char *rrr_http_upgrade_mode_str_websocket;
 extern const char *rrr_http_upgrade_mode_str_http2;
 
-#define RRR_HTTP_UPGRADE_MODE_TO_STR(transport)													\
-	(transport == RRR_HTTP_UPGRADE_MODE_NONE ? rrr_http_upgrade_mode_str_none :					\
-	(transport == RRR_HTTP_UPGRADE_MODE_WEBSOCKET ? rrr_http_upgrade_mode_str_websocket :		\
-	(transport == RRR_HTTP_UPGRADE_MODE_HTTP2 ? rrr_http_upgrade_mode_str_http2 : ("unknown")	\
-	)))
+#define RRR_HTTP_UPGRADE_MODE_TO_STR(transport)                                                                                \
+    (transport == RRR_HTTP_UPGRADE_MODE_NONE ? rrr_http_upgrade_mode_str_none :                                                \
+    (transport == RRR_HTTP_UPGRADE_MODE_WEBSOCKET ? rrr_http_upgrade_mode_str_websocket :                                      \
+    (transport == RRR_HTTP_UPGRADE_MODE_HTTP2 ? rrr_http_upgrade_mode_str_http2 : ("unknown")                                  \
+    )))
 
 extern const char *rrr_http_application_str_http1;
 extern const char *rrr_http_application_str_http2;
 
-#define RRR_HTTP_APPLICATION_TO_STR(transport)												\
-	(transport == RRR_HTTP_APPLICATION_HTTP1 ? rrr_http_application_str_http1 :				\
-	(transport == RRR_HTTP_APPLICATION_HTTP2 ? rrr_http_application_str_http2 : ("unknown")	\
-	))
+#define RRR_HTTP_APPLICATION_TO_STR(transport)                                                                                 \
+    (transport == RRR_HTTP_APPLICATION_HTTP1 ? rrr_http_application_str_http1 :                                                \
+    (transport == RRR_HTTP_APPLICATION_HTTP2 ? rrr_http_application_str_http2 : ("unknown")                                    \
+    ))
 
 typedef uint64_t rrr_http_unique_id;
 struct rrr_nullsafe_str;
 
-#define RRR_HTTP_COMMON_RECEIVE_RAW_CALLBACK_ARGS			\
-	const struct rrr_nullsafe_str *data,					\
-	struct rrr_http_transaction *transaction,				\
-	rrr_http_unique_id unique_id,							\
-	enum rrr_http_application_type next_protocol_version,	\
-	void *arg
+#define RRR_HTTP_COMMON_RECEIVE_RAW_CALLBACK_ARGS              \
+    const struct rrr_nullsafe_str *data,                       \
+    struct rrr_http_transaction *transaction,                  \
+    rrr_http_unique_id unique_id,                              \
+    enum rrr_http_application_type next_protocol_version,      \
+    void *arg
 
-#define RRR_HTTP_COMMON_DATA_MAKE_CALLBACK_ARGS \
+#define RRR_HTTP_COMMON_DATA_MAKE_CALLBACK_ARGS                \
 	const struct rrr_nullsafe_str *str, void *arg
 
 #endif /* RRR_HTTP_COMMON_H */
