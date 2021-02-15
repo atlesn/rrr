ca3a7a9ea10648bc2b2eb1dbad15e9a04ca2c51c
Atle Solbakken <atle@goliathdns.no>
2021-02-15 15:52:53 +0100

    Fix minor compilation and test issues on FreeBSD.

--- src/rrr_msgdb.c
+++ src/rrr_msgdb.c
@@ -21,6 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <fcntl.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
 #include "lib/log.h"
 #include "lib/common.h"
