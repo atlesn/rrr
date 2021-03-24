f26fe8ac7ccdd05d19975f9dd6fa0a5641641e5f
Atle Solbakken <atle@goliathdns.no>
2021-03-24 14:23:03 +0100

    Cleanup tabs.

--- src/lib/perl5/perl5_hv_macros.h
+++ src/lib/perl5/perl5_hv_macros.h
@@ -22,38 +22,31 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef RRR_PERL5_HV_MACROS_H
 #define RRR_PERL5_HV_MACROS_H
 
-#define RRR_PERL5_DEFINE_AND_FETCH_FROM_HV(name,hv_name)							\
-	SV *name = NULL;																\
-	do {SV **tmp = hv_fetch(hv, RRR_QUOTE(name), strlen(RRR_QUOTE(name)), 1);		\
-		if (tmp == NULL || *tmp == NULL) {											\
-			RRR_MSG_0("Could not fetch SV from HV\n");								\
-			ret = 1; goto out;														\
-		}																			\
-		name = *tmp; (void)(name);													\
-	} while(0)
-/*
-#define RRR_PERL5_CHECK_IS_AV(name)																				\
-	do {if (SvTYPE(name) != SVt_PVAV) {																			\
-		check_av_error_count++;																					\
-		RRR_MSG_0("Warning: " RRR_QUOTE(name) " was not a perl array while extracting array from perl5\n");		\
-	}} while (0)
-*/
-#define RRR_PERL5_DEFINE_AND_FETCH_FROM_AV(name,av_name,i)											\
-	SV *name = NULL;																				\
-	do {SV **tmp = av_fetch(av_name, i, 1);															\
-	if (tmp == NULL || *tmp == NULL) {																\
-		RRR_MSG_0("Could not fetch SV from array in __rrr_perl5_hv_to_message_extract_array\n");	\
-		ret = 1;																					\
-		goto out;																					\
-	}																								\
-	name = *tmp; } while(0)
+#define RRR_PERL5_DEFINE_AND_FETCH_FROM_HV(name,hv_name)                                                                       \
+    SV *name = NULL;                                                                                                           \
+    do {SV **tmp = hv_fetch(hv, RRR_QUOTE(name), strlen(RRR_QUOTE(name)), 1);                                                  \
+        if (tmp == NULL || *tmp == NULL) {                                                                                     \
+            RRR_MSG_0("Could not fetch SV from HV\n");                                                                         \
+            ret = 1; goto out;                                                                                                 \
+        }                                                                                                                      \
+        name = *tmp; (void)(name);                                                                                             \
+    } while(0)
 
-#define RRR_PERL5_DEFINE_AND_FETCH_ARRAY_PTR_FROM_HV(hv)				\
-	RRR_PERL5_DEFINE_AND_FETCH_FROM_HV(rrr_array_ptr, hv);				\
-	struct rrr_array *array = (struct rrr_array *) (intptr_t) SvIV(rrr_array_ptr);\
-	do {if ((intptr_t) array != SvIV(rrr_array_ptr) || array == NULL || SvIV(rrr_array_ptr) > INTPTR_MAX) { \
-		RRR_BUG("BUG: Invalid array pointer value retrieved from HV\n"); \
-	}} while(0)
+#define RRR_PERL5_DEFINE_AND_FETCH_FROM_AV(name,av_name,i)                                                                     \
+    SV *name = NULL;                                                                                                           \
+    do {SV **tmp = av_fetch(av_name, i, 1);                                                                                    \
+    if (tmp == NULL || *tmp == NULL) {                                                                                         \
+        RRR_MSG_0("Could not fetch SV from array in __rrr_perl5_hv_to_message_extract_array\n");                               \
+        ret = 1;                                                                                                               \
+        goto out;                                                                                                              \
+    }                                                                                                                          \
+    name = *tmp; } while(0)
 
+#define RRR_PERL5_DEFINE_AND_FETCH_ARRAY_PTR_FROM_HV(hv)                                                                       \
+    RRR_PERL5_DEFINE_AND_FETCH_FROM_HV(rrr_array_ptr, hv);                                                                     \
+    struct rrr_array *array = (struct rrr_array *) (intptr_t) SvIV(rrr_array_ptr);                                             \
+    do {if ((intptr_t) array != SvIV(rrr_array_ptr) || array == NULL || SvIV(rrr_array_ptr) > INTPTR_MAX) {                    \
+        RRR_BUG("BUG: Invalid array pointer value retrieved from HV\n");                                                       \
+    }} while(0)                                                                                                                \
 
 #endif /* RRR_PERL5_HV_MACROS_H */
