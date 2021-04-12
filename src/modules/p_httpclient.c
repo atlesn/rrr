9a7f3fca79012bbb032bfb9c08fb015a006f88bb
Atle Solbakken <atle@goliathdns.no>
2021-04-12 18:15:57 +0200

    Adjust loop_max

--- src/modules/p_httpclient.c
+++ src/modules/p_httpclient.c
@@ -1584,7 +1584,7 @@ static void httpclient_queue_process (
 		return;
 	}
 
-	int loop_max = 64;
+	int loop_max = 150;
 
 	// Check timeouts based on the time the loop starts to be fair
 	// if there are errors and the loop takes a while
