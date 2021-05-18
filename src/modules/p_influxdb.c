af852c870718443c1fa802025e4c6e065e2d4f68
Atle Solbakken <atle@goliathdns.no>
2021-05-18 21:29:47 +0200

    Add comment.

--- src/modules/p_influxdb.c
+++ src/modules/p_influxdb.c
@@ -531,6 +531,8 @@ static void *thread_entry_influxdb (struct rrr_thread *thread) {
 
 	RRR_DBG_1 ("InfluxDB started thread %p\n", thread_data);
 
+	/* Module must be converted to use events */
+
 	uint64_t timer_start = rrr_time_get_64();
 	while (rrr_thread_signal_encourage_stop_check(thread) == 0) {
 		rrr_thread_watchdog_time_update(thread);
