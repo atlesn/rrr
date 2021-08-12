9251e8cad09f96da742f3fde3840584376ad8d60
Atle Solbakken <atle@goliathdns.no>
2021-08-12 23:42:39 +0200

    Remove incorrect bug trap.

--- src/lib/cmodule/cmodule_main.c
+++ src/lib/cmodule/cmodule_main.c
@@ -131,9 +131,6 @@ int rrr_cmodule_main_worker_fork_start (
 	if (cmodule->worker_count == RRR_CMODULE_WORKER_MAX_WORKER_COUNT) {
 		RRR_BUG("BUG: Maximum worker count exceeded in rrr_cmodule_main_worker_fork_start\n");
 	}
-	if (cmodule->worker_count == 0) {
-		RRR_BUG("BUG: Maximum worker count was zero in rrr_cmodule_main_worker_fork_start\n");
-	}
 
 	struct rrr_cmodule_worker *worker = &cmodule->workers[cmodule->worker_count++];
 
