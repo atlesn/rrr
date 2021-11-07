836c47c3791224c064fd5114c365f5370072b810
Atle Solbakken <atle@goliathdns.no>
2021-11-07 17:54:50 +0100

    Use robust locks in mmap channel.

--- src/lib/cmodule/cmodule_channel.c
+++ src/lib/cmodule/cmodule_channel.c
@@ -45,9 +45,10 @@ static int __rrr_cmodule_mmap_channel_write_simple_callback (void *target, void 
 }
 
 int rrr_cmodule_channel_count (
+		int *count,
 		struct rrr_mmap_channel *channel
 ) {
-	return rrr_mmap_channel_count(channel);
+	return rrr_mmap_channel_count(count, channel);
 }
 
 int rrr_cmodule_channel_send_message_simple (
