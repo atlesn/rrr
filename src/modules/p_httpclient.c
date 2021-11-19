3f115130faa62bcf0632f01770301dded07a4eaa
LBSdrift <lbsdrift@posten.local>
2021-11-19 11:29:47 +0100

    Move check of whether silent put error is to be performed to outside entry lock.

--- src/modules/p_httpclient.c
+++ src/modules/p_httpclient.c
@@ -856,12 +856,10 @@ static int httpclient_final_callback (
 
 		int do_print_error = 1;
 
-		if (transaction->method == RRR_HTTP_METHOD_PUT) {
+		if (transaction->method == RRR_HTTP_METHOD_PUT && httpclient_data->silent_put_error_limit_us != 0) {
 			rrr_msg_holder_lock(transaction_data->entry);
 
-			if ( httpclient_data->silent_put_error_limit_us != 0 &&
-			     rrr_time_get_64() < ((struct rrr_msg_msg *) transaction_data->entry->message)->timestamp + httpclient_data->silent_put_error_limit_us
-			) {
+			if (rrr_time_get_64() < ((struct rrr_msg_msg *) transaction_data->entry->message)->timestamp + httpclient_data->silent_put_error_limit_us) {
 				RRR_DBG_4("Error response %i for PUT query temporarily ignored per configuration\n", transaction->response_part->response_code);
 				do_print_error = 0;
 			}
