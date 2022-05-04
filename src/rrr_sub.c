6f21c455977f058310144719025f36c1f1fb43dc
Atle Solbakken <atle@goliathdns.no>
2022-05-04 23:28:13 +0200

    Add initial RRR MQTT sub command.

---
+++ src/rrr_sub.c
@@ -0,0 +1,361 @@
+/*
+
+Read Route Record
+
+Copyright (C) 2022 Atle Solbakken atle@goliathdns.no
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+/*
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "lib/rrr_config.h"
+#include "lib/rrr_strerror.h"
+#include "lib/array.h"
+#include "lib/helpers/string_builder.h"
+#include "lib/messages/msg.h"
+#include "lib/messages/msg_msg.h"
+#include "lib/messages/msg_addr.h"
+#include "lib/messages/msg_log.h"
+#include "lib/messages/msg_checksum.h"
+#include "lib/ip/ip_defines.h"
+#include "lib/util/rrr_endian.h"
+*/
+
+#include "../build_timestamp.h"
+#include "main.h"
+#include "lib/allocator.h"
+#include "lib/common.h"
+#include "lib/version.h"
+#include "lib/log.h"
+#include "lib/cmdlineparser/cmdline.h"
+#include "lib/mqtt/mqtt_subscription.h"
+#include "lib/mqtt/mqtt_client.h"
+#include "lib/socket/rrr_socket.h"
+#include "lib/net_transport/net_transport_config.h"
+#include "lib/util/rrr_time.h"
+
+#define RRR_MQTT_SUB_TOPICS_MAX 64
+#define RRR_MQTT_DISCONNECT_TIMEOUT_S 5
+
+RRR_CONFIG_DEFINE_DEFAULT_LOG_PREFIX("rrr_sub");
+
+static const struct cmd_arg_rule cmd_rules[] = {
+        {CMD_ARG_FLAG_NO_FLAG_MULTI,   '\0',   "topic",                "[TOPIC]..."},
+//        {0,                            'b',    "broker",               "[-b|--broker]"},
+/*      {0,                            'p',    "port",                 "[-p|--port]"},
+        {0,                            'f',    "force",                "[-f|--force]"},
+        {0,                            's',    "selftest",             "[-s|--selftest]"}, */
+        {0,                            'l',    "loglevel-translation", "[-l|--loglevel-translation]"},
+        {0,                            'b',    "banner",               "[-b|--banner]"},
+        {CMD_ARG_FLAG_HAS_ARGUMENT,    'e',    "environment-file",     "[-e|--environment-file[=]ENVIRONMENT FILE]"},
+        {CMD_ARG_FLAG_HAS_ARGUMENT,    'd',    "debuglevel",           "[-d|--debuglevel[=]DEBUG FLAGS]"},
+        {CMD_ARG_FLAG_HAS_ARGUMENT,    'D',    "debuglevel-on-exit",   "[-D|--debuglevel-on-exit[=]DEBUG FLAGS]"},
+        {0,                            'h',    "help",                 "[-h|--help]"},
+        {0,                            'v',    "version",              "[-v|--version]"},
+        {0,                            '\0',    NULL,                  NULL}
+};
+
+struct rrr_sub_data {
+	struct rrr_mqtt_subscription_collection topics;
+	char *broker;
+	unsigned int port;
+
+	struct rrr_mqtt_property_collection connect_properties;
+	struct rrr_mqtt_client_data *mqtt_client;
+
+	int transport_handle;
+	struct rrr_mqtt_session *session;
+
+	uint64_t disconnect_time;
+	uint64_t subscriptions_send_time
+};
+
+static void __rrr_sub_data_cleanup (
+		struct rrr_sub_data *data
+) {
+	RRR_FREE_IF_NOT_NULL(data->broker);
+	rrr_mqtt_client_destroy(data->mqtt_client);
+	rrr_mqtt_subscription_collection_clear(&data->topics);
+	rrr_mqtt_property_collection_clear(&data->connect_properties);
+}
+
+static int __rrr_sub_suback_unsuback_handler (struct rrr_mqtt_client_data *client, struct rrr_mqtt_p_suback_unsuback *packet, void *arg) {
+	int ret = RRR_MQTT_OK;
+	return ret;
+}
+
+static int __rrr_sub_packet_parsed_handler (struct rrr_mqtt_client_data *client, struct rrr_mqtt_p *packet, void *arg) {
+	int ret = RRR_MQTT_OK;
+	return ret;
+}
+
+static void __rrr_sub_receive_publish (struct rrr_mqtt_p_publish *publish, void *arg) {
+}
+
+static int __rrr_sub_init (
+		struct rrr_sub_data *data,
+		struct cmd_data *cmd,
+		struct rrr_event_queue *events,
+		struct rrr_mqtt_common_init_data *mqtt_init_data
+) {
+	int ret = 0;
+
+	if ((data->broker = strdup("localhost")) == NULL) {
+		RRR_MSG_0("Failed to allocate memory for broker in %s\n", __func__);
+		ret = 1;
+		goto out;
+	}
+
+	data->port = 1883;
+
+	for (unsigned long i = 0; i <= RRR_MQTT_SUB_TOPICS_MAX; i++) {
+		if (i == RRR_MQTT_SUB_TOPICS_MAX) {
+			RRR_MSG_0("Too many topics given (max is %i)\n", RRR_MQTT_SUB_TOPICS_MAX);
+			ret = 1;
+			goto out_cleanup_broker;
+		}
+		if (cmd_exists(cmd, "topic", i)) {
+			const char *topic = cmd_get_value(cmd, "topic", i);
+			if ((ret = rrr_mqtt_subscription_collection_push_unique_str (
+					&data->topics,
+					topic,
+					0,
+					0,
+					0,
+					0 // QoS
+			)) != 0) {
+				goto out;
+			}
+		}
+		else {
+			break;
+		}
+	}
+
+	if ((ret = rrr_mqtt_client_new (
+			&data->mqtt_client,
+			mqtt_init_data,
+			events,
+			rrr_mqtt_session_collection_ram_new_client,
+			NULL,
+			__rrr_sub_suback_unsuback_handler,
+			&data,
+			__rrr_sub_packet_parsed_handler,
+			&data,
+			__rrr_sub_receive_publish,
+			&data
+	)) != 0) {
+		goto out_cleanup_topic;
+	}
+
+	goto out;
+	out_cleanup_topic:
+		rrr_mqtt_subscription_collection_clear(&data->topics);
+	out_cleanup_broker:
+		RRR_FREE_IF_NOT_NULL(data->broker);
+	out:
+		return ret;
+}
+
+static int main_running = 1;
+
+int rrr_signal_handler(int s, void *arg) {
+	return rrr_signal_default_handler(&main_running, s, arg);
+}
+
+static int __rrr_sub_periodic(RRR_EVENT_FUNCTION_PERIODIC_ARGS) {
+	struct rrr_sub_data *data = arg;
+
+	int alive = 0;
+	int send_allowed = 0;
+	int close_wait = 0;
+
+	if (data->transport_handle > 0 && rrr_mqtt_client_connection_check_alive (
+			&alive,
+			&send_allowed,
+			&close_wait,
+			data->mqtt_client,
+			data->transport_handle
+	) != 0) {
+		return RRR_EVENT_ERR;
+	}
+
+	printf("alive %i send allowed %i close wait %i\n", alive, send_allowed, close_wait);
+
+	if (data->disconnect_time > 0) {
+		if (!alive && !close_wait) {
+			RRR_DBG_1("Disconnect complete\n");
+			return RRR_EVENT_EXIT;
+		}
+		else if (rrr_time_get_64() > data->disconnect_time + RRR_MQTT_DISCONNECT_TIMEOUT_S * 1000 * 1000) {
+			RRR_DBG_1("Disconnect timeout\n");
+			return RRR_EVENT_EXIT;
+		}
+	}
+	else if (!alive) {
+		RRR_DBG_1("Connecting to broker...\n");
+
+		data->subscriptions_send_time = 0;
+		data->disconnect_time = 0;
+
+		int ret_tmp;
+		if ((ret_tmp = rrr_mqtt_client_connect (
+				&data->transport_handle,
+				&data->session,
+				data->mqtt_client,
+				data->broker,
+				(uint16_t) data->port,
+				4,    // Version 3.1.1
+				30,   // Keep-alive
+				1,    // Clean start,
+				NULL, // Username
+				NULL, // Password
+				&data->connect_properties,
+				NULL, // Will topic
+				NULL, // Will message
+				0,    // Will QoS
+				0     // Will retain
+		)) != 0) {
+			if (ret_tmp == RRR_MQTT_SOFT_ERROR) {
+				RRR_MSG_0("Connection to %s:%i failed, trying again\n",
+					data->broker, data->port);
+			}
+			else {
+				RRR_MSG_0("Connection to %s:%i failed, critical error\n",
+					data->broker, data->port);
+				return RRR_EVENT_ERR;
+			}
+		}
+	}
+	else if (send_allowed) {
+		if (!main_running) {
+			RRR_DBG_1("Disconnecting...\n");
+			data->disconnect_time = rrr_time_get_64();
+			if (rrr_mqtt_client_disconnect(data->mqtt_client, data->transport_handle, 0) != 0) {
+				return RRR_EVENT_EXIT;
+			}
+		}
+		else if (!data->subscriptions_send_time) {
+			RRR_DBG_1("Sending subscriptions...\n");
+			data->subscriptions_send_time = rrr_time_get_64();
+		}
+	}
+	else if (!main_running) {
+		RRR_DBG_1("Exiting\n");
+		return RRR_EVENT_EXIT;
+	}
+
+	return RRR_EVENT_OK;
+}
+
+int main (int argc, const char **argv, const char **env) {
+	if (!rrr_verify_library_build_timestamp(RRR_BUILD_TIMESTAMP)) {
+		fprintf(stderr, "Library build version mismatch.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	int ret = EXIT_SUCCESS;
+
+	struct rrr_signal_handler *signal_handler = NULL;
+	struct rrr_net_transport_config net_transport_config = RRR_NET_TRANSPORT_CONFIG_PLAIN_INITIALIZER;
+
+	if (rrr_allocator_init() != 0) {
+		ret = EXIT_FAILURE;
+		goto out_destroy_net_transport_config;
+	}
+
+	if (rrr_log_init() != 0) {
+		ret = EXIT_FAILURE;
+		goto out_cleanup_allocator;
+	}
+
+	//rrr_strerror_init();
+	rrr_signal_default_signal_actions_register();
+	signal_handler = rrr_signal_handler_push(rrr_signal_handler, NULL);
+	rrr_signal_handler_set_active (RRR_SIGNALS_ACTIVE);
+
+	struct cmd_data cmd;
+	struct rrr_sub_data data = {0};
+	struct rrr_event_queue *events = NULL;
+	struct rrr_mqtt_common_init_data mqtt_init_data = {
+		NULL, /* Client name */
+		RRR_MQTT_COMMON_RETRY_INTERVAL_S * 1000 * 1000,
+		RRR_MQTT_COMMON_CLOSE_WAIT_TIME_S * 1000 * 1000,
+		RRR_MQTT_COMMON_MAX_CONNECTIONS
+	};
+
+	cmd_init(&cmd, cmd_rules, argc, argv);
+
+	if (rrr_main_parse_cmd_arguments_and_env(&cmd, env, CMD_CONFIG_DEFAULTS) != 0) {
+		ret = EXIT_FAILURE;
+		goto out_cleanup_allocator;
+	}
+
+	if (rrr_main_print_banner_help_and_version(&cmd, 1) != 0) {
+		goto out_cleanup_cmd;
+	}
+
+	if (rrr_event_queue_new(&events) != 0) {
+		ret = EXIT_FAILURE;
+		goto out_cleanup_cmd;
+	}
+
+	if (__rrr_sub_init (
+			&data,
+			&cmd,
+			events,
+			&mqtt_init_data
+	) != 0) {
+		ret = EXIT_FAILURE;
+		goto out_destroy_event;
+	}
+
+	if (rrr_mqtt_client_start(data.mqtt_client, &net_transport_config) != 0) {
+		ret = EXIT_FAILURE;
+		goto out_cleanup_data;
+	}
+
+	if (rrr_event_dispatch (
+			events,
+			250 * 1000, // 250 ms
+			__rrr_sub_periodic,
+			&data
+	) != 0) {
+		ret = EXIT_FAILURE;
+		goto out_destroy_event;
+	}
+
+	rrr_signal_handler_set_active(RRR_SIGNALS_NOT_ACTIVE);
+	rrr_signal_handler_remove(signal_handler);
+	rrr_config_set_debuglevel_on_exit();
+
+	out_cleanup_data:
+		__rrr_sub_data_cleanup(&data);
+	out_destroy_event:
+		rrr_event_queue_destroy(events);
+	out_cleanup_cmd:
+		cmd_destroy(&cmd);
+		rrr_socket_close_all();
+		//rrr_strerror_cleanup();
+	out_cleanup_allocator:
+		rrr_allocator_cleanup();
+	out_destroy_net_transport_config:
+		rrr_net_transport_config_cleanup(&net_transport_config);
+		return ret;
+}
