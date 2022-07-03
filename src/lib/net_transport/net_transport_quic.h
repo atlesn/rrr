bfedaeff5076d3592e220219cd1e7df67b810e54
Atle Solbakken <atle@goliathdns.no>
2022-07-03 12:48:20 +0200

    Cleanup debug messages.

--- src/lib/net_transport/net_transport_quic.h
+++ src/lib/net_transport/net_transport_quic.h
@@ -22,24 +22,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef RRR_NET_TRANSPORT_QUIC_H
 #define RRR_NET_TRANSPORT_QUIC_H
 
-#include "net_transport_tls_common.h"
+#include "net_transport.h"
 
 struct rrr_net_transport_tls;
-typedef struct rrr_net_transport_quic_vec rrr_net_transport_quic_vec;
-
-typedef int (*rrr_net_transport_quic_cb_ready)(void *arg);
-typedef int (*rrr_net_transport_quic_cb_get_data)(int64_t *stream_id, rrr_net_transport_quic_vec *vec, size_t *vec_count, int *fin, void *arg);
-typedef int (*rrr_net_transport_quic_cb_ack_data)(int64_t stream_id, size_t bytes, void *arg);
-typedef int (*rrr_net_transport_quic_cb_deliver_data)(size_t *consumed, int64_t stream_id, const uint8_t *buf, size_t buflen, int fin, void *arg);
-typedef int (*rrr_net_transport_quic_cb_block_stream)(int64_t stream_id, int blocked, void *arg);
-
-struct rrr_net_transport_quic_callbacks {
-	rrr_net_transport_quic_cb_ready cb_ready;
-	rrr_net_transport_quic_cb_get_data cb_get_data;
-	rrr_net_transport_quic_cb_ack_data cb_ack_data;
-	rrr_net_transport_quic_cb_deliver_data cb_deliver_data;
-	rrr_net_transport_quic_cb_block_stream cb_block_stream;
-};
 
 int rrr_net_transport_quic_new (
 		struct rrr_net_transport_tls **target,
