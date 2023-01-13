4201840ab55d08ed3cb4ebdc878578b59bdc969f
Atle Solbakken <atle@goliathdns.no>
2023-01-13 19:14:36 +0100

    Create misc. V8 wrapper classes.

--- cpp-nodejs/test.cxx
+++ cpp-nodejs/test.cxx
@@ -17,18 +17,90 @@ namespace RRR::JS {
 		isolate_create_params.array_buffer_allocator = v8::ArrayBuffer::Allocator::NewDefaultAllocator();
 		isolate = v8::Isolate::New(isolate_create_params);
 	}
+
 	ENV::~ENV() {
 		isolate->Dispose();
 		delete isolate_create_params.array_buffer_allocator;
 
 		v8::V8::Dispose();
 	}
+
+	ENV::operator v8::Isolate *() {
+		return isolate;
+	}
+
+	SCOPE::SCOPE(ENV &env) :
+		isolate(env.isolate),
+		isolate_scope(isolate),
+		handle_scope(isolate)
+	{
+	}
+
+	CTX::CTX(ENV &env) :
+		ctx(v8::Context::New(env)),
+		ctx_scope(ctx)
+	{
+	}
+
+	CTX::operator v8::Local<v8::Context> () {
+		return ctx;
+	}
+
+	String::String(ENV &env, const char *str) :
+		str(v8::String::NewFromUtf8(env, str, v8::NewStringType::kNormal).ToLocalChecked())
+	{
+	}
+
+	String::operator v8::Local<v8::String> () {
+		return str;
+	}
+
+	Value::Value(v8::Local<v8::Value> value) :
+		v8::Local<v8::Value>()
+	{
+	}
+
+	UTF8::UTF8(ENV &env, Value &value) :
+		utf8(env, value) {
+	}
+
+	UTF8::UTF8(ENV &env, Value &&value) :
+		utf8(env, value) {
+	}
+
+	const char * UTF8::operator * () {
+		return *utf8;
+	}
+
+	Script::Script(CTX &ctx, String &str) :
+		ctx(ctx),
+		script(v8::Script::Compile(ctx, str).ToLocalChecked())
+	{
+	}
+
+	Script::Script(CTX &ctx, String &&str) :
+		ctx(ctx),
+		script(v8::Script::Compile(ctx, str).ToLocalChecked())
+	{
+	}
+
+	Value Script::run() {
+		return script->Run(ctx).ToLocalChecked();
+	}
 } // namespace RRR::JS
 
 int main(int argc, const char **argv) {
-	RRR::JS::ENV js(*argv);
+	using namespace RRR::JS;
+
+	ENV env(*argv);
+
 	{
-	//	auto script_str = v8::String::NewFromUtf8(scope, script, v8::String::NewStringType::kNormalString, sizeof(script) - 1);
+		auto scope = SCOPE(env);
+		auto ctx = CTX(env);
+		auto script = Script(ctx, String(env, "'Hello';"));
+		auto result = UTF8(env, script.run());
+
+		printf("Result: %s\n", *result);
 	}
 
 	return 0;
--- cpp-nodejs/test.hxx
+++ cpp-nodejs/test.hxx
@@ -1,10 +1,13 @@
 #pragma once
 
+#include "v8-isolate.h"
 #include <memory>
 #include <v8.h>
 
 namespace RRR::JS {
 	class ENV {
+		friend class SCOPE;
+
 		private:
 		std::unique_ptr<v8::Platform> platform;
 		v8::Isolate::CreateParams isolate_create_params;
@@ -13,5 +16,61 @@ namespace RRR::JS {
 		public:
 		ENV(const char *program_name);
 		~ENV();
+		operator v8::Isolate *();
+	};
+
+	class SCOPE {
+		private:
+		v8::Isolate *isolate;
+		v8::Isolate::Scope isolate_scope;
+		v8::HandleScope handle_scope;
+
+		public:
+		SCOPE(ENV &env);
+	};
+
+	class CTX {
+		private:
+		v8::Local<v8::Context> ctx;
+		v8::Context::Scope ctx_scope;
+
+		public:
+		CTX(ENV &env);
+		operator v8::Local<v8::Context>();
+	};
+
+	class String {
+		private:
+		v8::Local<v8::String> str;
+
+		public:
+		String(ENV &env, const char *str);
+		operator v8::Local<v8::String>();
+	};
+
+	class Value : public v8::Local<v8::Value> {
+		public:
+		Value(v8::Local<v8::Value> value);
+	};
+
+	class UTF8 {
+		private:
+		v8::String::Utf8Value utf8;
+
+		public:
+		UTF8(ENV &env, Value &value);
+		UTF8(ENV &env, Value &&value);
+		const char * operator *();
+	};
+
+	class Script {
+		private:
+		v8::Local<v8::Context> ctx;
+		v8::Local<v8::Script> script;
+
+		public:
+		Script(CTX &ctx, String &str);
+		Script(CTX &ctx, String &&str);
+		Value run();
 	};
 }
