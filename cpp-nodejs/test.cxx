a93daccadf2aa36f0741d0445665368ea889d680
Atle Solbakken <atle@goliathdns.no>
2023-01-13 18:13:20 +0100

    Fix header file.

--- cpp-nodejs/test.cxx
+++ cpp-nodejs/test.cxx
@@ -1,19 +1,12 @@
-#include <iostream>
 #include "test.hxx"
-#include <libplatform/libplatform.h>
 #include <v8.h>
-#include <v8-platform.h>
+#include <libplatform/libplatform.h>
+//#include <v8-platform.h>
 
 const char script[] = "function(){ return true; }";
 
-class RRRJS {
-	private:
-	std::unique_ptr<v8::Platform> platform;
-	v8::Isolate::CreateParams isolate_create_params;
-	v8::Isolate *isolate;
-
-	public:
-	RRRJS(const char *program_name) :
+namespace RRR::JS {
+	ENV::ENV(const char *program_name) :
 		platform(v8::platform::NewDefaultPlatform())
 	{
 		v8::V8::InitializeICUDefaultLocation(program_name);
@@ -24,16 +17,16 @@ class RRRJS {
 		isolate_create_params.array_buffer_allocator = v8::ArrayBuffer::Allocator::NewDefaultAllocator();
 		isolate = v8::Isolate::New(isolate_create_params);
 	}
-	~RRRJS() {
+	ENV::~ENV() {
 		isolate->Dispose();
 		delete isolate_create_params.array_buffer_allocator;
 
 		v8::V8::Dispose();
 	}
-};
+} // namespace RRR::JS
 
 int main(int argc, const char **argv) {
-	RRRJS js(*argv);
+	RRR::JS::ENV js(*argv);
 	{
 	//	auto script_str = v8::String::NewFromUtf8(scope, script, v8::String::NewStringType::kNormalString, sizeof(script) - 1);
 	}
--- cpp-nodejs/test.hxx
+++ cpp-nodejs/test.hxx
@@ -1,4 +1,17 @@
 #pragma once
 
+#include <memory>
+#include <v8.h>
+
 namespace RRR::JS {
+	class ENV {
+		private:
+		std::unique_ptr<v8::Platform> platform;
+		v8::Isolate::CreateParams isolate_create_params;
+		v8::Isolate *isolate;
+
+		public:
+		ENV(const char *program_name);
+		~ENV();
+	};
 }
