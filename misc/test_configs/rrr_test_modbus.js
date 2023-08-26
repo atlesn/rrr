e76a364d3f872ff4897e0488f8854e413f6af2d1
Atle Solbakken <atle@goliathdns.no>
2023-08-26 19:52:53 +0200

    Improve language

--- misc/test_configs/rrr_test_modbus.js
+++ misc/test_configs/rrr_test_modbus.js
@@ -3,5 +3,6 @@ console.log("Running module!!!!!!!\n");
 export function source(message) {
 	message.push_tag("modbus_server", "localhost");
 	message.push_tag("modbus_interval_ms", "2");
+	message.push_tag("modbus_function", "1");
 	message.send();
 }
