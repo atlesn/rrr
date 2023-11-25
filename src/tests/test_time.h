fe2feba424f2ba5a66dbffae5d17e91b9314fa0f
Atle Solbakken <atle@goliathdns.no>
2023-11-25 18:45:03 +0100

    Rename usleep test to time.

--- src/tests/test_time.h
+++ src/tests/test_time.h
@@ -21,6 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef RRR_TEST_USLEEP_H
 #define RRR_TEST_USLEEP_H
 
-int rrr_test_usleep (void);
+int rrr_test_time (void);
 
 #endif /* RRR_TEST_USLEEP_H */
