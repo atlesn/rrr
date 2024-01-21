ab2f6e261d48370ea75fb4c1027ad26c4f8f5e34
Atle Solbakken <atle@goliathdns.no>
2024-01-21 09:54:51 +0100

    [postgresql] Add create table SQL

---
+++ misc/postgresql/test_db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS "rrr-test-array-types-2" (
+	"int1" bigint NOT NULL,
+	"int2" bigint NOT NULL,
+	"int3" bigint NOT NULL,
+	"int4" bigint NOT NULL,
+	"int5" bigint NOT NULL,
+	"int6" bigint NOT NULL,
+	"int7" bigint NOT NULL,
+	"int8" bigint NOT NULL,
+	"rrr_msg_msg" bytea NOT NULL,
+	"blob_combined" bytea NOT NULL,
+	"timestamp" bigint NOT NULL
+);
