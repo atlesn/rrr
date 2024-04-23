d6477bf4293f8de37b2e7276a733a08c3a589a64
LBSdrift <lbsdrift@posten.local>
2024-04-23 09:34:45 +0200

    [js] Add missing include

--- src/lib/js/Js.cxx
+++ src/lib/js/Js.cxx
@@ -36,6 +36,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <algorithm>
 #include <set>
 #include <filesystem>
+#include <array>
 
 namespace RRR::JS {
 	ENV::ENV(const char *program_name) :
