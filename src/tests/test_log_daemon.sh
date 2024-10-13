bb989fa3f54c583e0b0821a3f81212d471f7db63
Atle Solbakken <atle@goliathdns.no>
2024-10-13 08:12:49 +0000

    Refactor log daemon test

--- src/tests/test_log_daemon.sh
+++ src/tests/test_log_daemon.sh
@@ -4,7 +4,10 @@
 set +e
 
 VALGRIND=
-EXTRA_ARGS=
+LOGD=../.libs/rrr_logd
+PID=
+POST=../.libs/rrr_post
+EXTRA_ARGS=""
 SOCKET=/tmp/rrr-logd-socket.sock
 TMP=/tmp/rrr-logd-output
 LOG_MSG="DEADBEEF"
@@ -21,17 +24,22 @@ else
 	SLEEP=0.2
 fi
 
-sigkill_and_bail() {
-	PID=$1
-	MSG=$2
-	kill -SIGKILL $PID
+rm -f $SOCKET || exit 1
+
+bail() {
+	MSG=$1
 	echo $MSG
 	exit 1
 }
 
+sigkill_and_bail() {
+	MSG=$1
+	kill -SIGKILL $PID
+	bail "$MSG"
+}
+
 function wait_and_verify() {
-	PID=$1
-	EXPECTED_RET=$2
+	EXPECTED_RET=$1
 
 	wait $PID
 	RET=$?
@@ -46,82 +54,74 @@ function wait_and_verify() {
 
 function verify_socket_deleted() {
 	if [ -e $SOCKET ]; then
-		echo "Socket $SOCKET still existed"
-		exit 1
+		sigkill_and_bail $PID "Socket $SOCKET still existed"
 	fi
 
 	return 0
 }
 
 function verify_socket_exists() {
-	if [ -e $SOCKET ]; then
-		echo "Socket $SOCKET still existed"
-		exit 1
+	if ! [ -S $SOCKET ]; then
+		sigkill_and_bail $PID "Socket $SOCKET did not exist"
 	fi
 
 	return 0
 }
 
-####################################################
-# Verify that logd terminates neatly
-####################################################
+function logd_start() {
+	ARGS=$1
+	$VALGRIND $LOGD -p $ARGS $EXTRA_ARGS -s $SOCKET > $TMP &
+	PID=$!
+	sleep $SLEEP
+}
 
-rm -f $SOCKET || exit 1
+function logd_stop() {
+	EXPECTED_RET=$1
+	kill -SIGUSR1 $PID || exit $?
+	wait_and_verify $EXPECTED_RET
+	PID=
+	verify_socket_deleted
+}
 
-$VALGRIND ../.libs/rrr_logd -s $SOCKET $EXTRA_ARGS &
-PID=$!
+function logd_output() {
+	OUTPUT=`cat $TMP`
+	rm -f $TMP
+	echo "$OUTPUT"
+}
 
-sleep $SLEEP
-
-if ! [ -S $SOCKET ]; then
-	sigkill_and_bail $PID "Socket $SOCKET did not exist"
-fi
-
-kill -SIGUSR1 $PID || exit $?
-wait_and_verify $PID 0
-RET=$?
-
-verify_socket_deleted
-
-####################################################
-# Verify that incorrect fd produces error
-####################################################
-
-$VALGRIND ../.libs/rrr_logd -f 666 $EXTRA_ARGS &
-PID=$!
-
-sleep $SLEEP
-
-kill -SIGKILL $PID > /dev/null 2>&1
-
-wait_and_verify $PID 1
+function logd_start_stop() {
+	ARGS=$1
+	EXPECTED_RET=$2
+	logd_start "$ARGS"
+	if [ $EXPECTED_RET -eq 0 ]; then
+		verify_socket_exists
+		logd_stop $EXPECTED_RET
+	else
+		if kill -SIGKILL $PID; then
+			bail "Process did not terminate early although expected"
+		fi
+	fi
+}
 
 function log_delivery() {
 	IN=$1
 	POST_ARGS=$2
 	OUT=$3
 
-	# Don't add $EXTRA_ARGS here, clobbers output
-	$VALGRIND ../.libs/rrr_logd -p -s $SOCKET > $TMP &
-	PID=$!
-
-	sleep $SLEEP
+	logd_start
+	verify_socket_exists
 
 	# Test using specified array definition
-	if ! echo "$IN" | ../.libs/rrr_post $SOCKET $POST_ARGS -c 1 -f - $EXTRA_ARGS; then
-		sigkill_and_bail $PID "Failed to post log messages"
+	if ! echo "$IN" | $POST $SOCKET $POST_ARGS -c 1 -f - $EXTRA_ARGS; then
+		sigkill_and_bail "Failed to post log messages"
 		exit 1
 	fi
 
 	sleep $SLEEP
 
-	kill -SIGUSR1 $PID || exit $?
-	wait_and_verify $PID 0
-	RET=$?
+	logd_stop 0
 
-	verify_socket_deleted
-
-	OUTPUT=`cat $TMP`
+	OUTPUT=`logd_output`
 
 	if [ "x$OUTPUT" != "x$OUT" ]; then
 		echo "Unexpected output '$OUTPUT' expected '$OUT'"
@@ -131,6 +131,17 @@ function log_delivery() {
 
 	rm -f $TMP
 }
+
+####################################################
+# Verify that logd terminates neatly
+####################################################
+
+logd_start_stop "" 0
+
+####################################################
+# Verify that incorrect fd produces error
+####################################################
+logd_start_stop "-f 666" 1
 
 ####################################################
 # Verify array log message delivery on socket
@@ -143,4 +154,9 @@ log_delivery "$LOG_IN" "-a $LOG_DEF" "$LOG_OUT_4"
 ####################################################
 
 log_delivery "$LOG_MSG" "-L" "$LOG_OUT_7" 
+
+####################################################
+# Verify log message delivery from main process
+####################################################
+
 
