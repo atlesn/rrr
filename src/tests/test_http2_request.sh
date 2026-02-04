8ebe91f2cca18c8d0e518412a908a70d4acb67b9
Atle Solbakken <atle@goliathdns.no>
2026-02-04 09:32:57 +0100

    [http2] Fix test case

--- src/tests/test_http2_request.sh
+++ src/tests/test_http2_request.sh
@@ -6,7 +6,7 @@ source ./testlib.sh
 source ../../variables.sh
 
 if test "x$RRR_WITH_NGHTTP2" != 'xno'; then
-	do_test_socket test_http2_request.conf
+	do_test_simple test_http2_request.conf
 else
 	echo "Skipped test_http2_request.conf as HTTP2 support is missing"
 fi
